ffb4f0fa85114d1a4a0cd989741f919d583b5794
Cassin01 <koyojb@gmail.com>
2019-12-20 21:39:30 +0900

    align

--- cmm/code.h
+++ cmm/code.h
@@ -6,24 +6,24 @@
 
 typedef
 struct CODE {
-  struct CODE *next;
-  int f, l, a;
+    struct CODE *next;
+    int f, l, a;
 } code;
 
 typedef struct CPTR {
-  code *h;
-  code *t;
+    code *h;
+    code *t;
 } cptr;
 
 /* operation codes of PL/0 code */
-typedef enum { 
-  O_LIT, O_OPR, O_LOD, O_STO, O_CAL, O_INT, 
-  O_JMP, O_JPC, O_CSP, O_LAB, O_BAD, O_RET
+typedef enum {
+    O_LIT, O_OPR, O_LOD, O_STO, O_CAL, O_INT,
+    O_JMP, O_JPC, O_CSP, O_LAB, O_BAD, O_RET
 } opecode;
 /* mnemonic code */
 typedef struct {
-  char *sym; 
-  int cd;
+    char *sym;
+    int cd;
 } mnemonic;
 
 
