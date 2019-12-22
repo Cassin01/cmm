0907a8e336313ba6b249a6c59410cd9502496ae3
Cassin01 <koyojb@gmail.com>
2019-12-22 13:37:00 +0900

    add pow

--- cmm/code.c
+++ cmm/code.c
@@ -5,72 +5,91 @@
 
 /* table for mnemonic code */
 mnemonic mntbl[] = {
-  { "LIT", O_LIT }, { "OPR", O_OPR },
-  { "LOD", O_LOD }, { "STO", O_STO },
-  { "CAL", O_CAL }, { "INT", O_INT },
-  { "JMP", O_JMP }, { "JPC", O_JPC },
-  { "CSP", O_CSP }, { "LAB", O_LAB },
-  { "   ", O_BAD }, { "RET", O_RET }
+    { "LIT", O_LIT }, { "OPR", O_OPR },
+    { "LOD", O_LOD }, { "STO", O_STO },
+    { "CAL", O_CAL }, { "INT", O_INT },
+    { "JMP", O_JMP }, { "JPC", O_JPC },
+    { "CSP", O_CSP }, { "LAB", O_LAB },
+    { "   ", O_BAD }, { "RET", O_RET }
 };
 
 
-cptr *makecode(int f, int l, int a){
-  code *tmp;
-  cptr *tmp2;
+cptr *makecode(int f, int l, int a) {
+    code *tmp;
+    cptr *tmp2;
 
-  tmp = (code*)malloc(sizeof(code));
-  if (tmp == NULL){
-    perror("memory allocation");
-    exit(EXIT_FAILURE);
-  }
+    tmp = (code*)malloc(sizeof(code));
+    if (tmp == NULL){
+        perror("memory allocation");
+        exit(EXIT_FAILURE);
+    }
 
-  tmp->f = f;
-  tmp->l = l;
-  tmp->a = a;
-  tmp->next = NULL;
+    tmp->f = f;
+    tmp->l = l;
+    tmp->a = a;
+    tmp->next = NULL;
 
-  tmp2 = (cptr*)malloc(sizeof(cptr));
-  if (tmp == NULL){
-    perror("memory allocation");
-    exit(EXIT_FAILURE);
-  }
-  
-  tmp2->h = tmp2->t = tmp;
+    tmp2 = (cptr*)malloc(sizeof(cptr));
+    if (tmp == NULL){
+        perror("memory allocation");
+        exit(EXIT_FAILURE);
+    }
 
-  return tmp2;
+    tmp2->h = tmp2->t = tmp;
+
+    return tmp2;
 }
 
 
 cptr* mergecode(cptr* c1, cptr* c2){
 
-  if (c1 ==  NULL){
-    return c2;
-  }
+    if (c1 ==  NULL) {
+        return c2;
+    }
 
-  if (c2 == NULL){
+    if (c2 == NULL) {
+        return c1;
+    }
+
+    c1->t->next = c2->h;
+    c1->t = c2->t;
+
+    free(c2);
+
     return c1;
-  }
-  
-  c1->t->next = c2->h;
-  c1->t = c2->t;
-    
-  free(c2);
-    
-  return c1;
 }
 
-void printcode(FILE* f, cptr* c){
-  code* tmp;
+void printcode(FILE* f, cptr* c) {
+    code* tmp;
 
-  for(tmp=c->h; tmp != NULL; tmp = tmp->next){
-    fprintf(f, "( %s, %4d, %4d )\n",
-	    mntbl[tmp->f].sym, tmp->l, tmp->a);
-  }
+    for(tmp=c->h; tmp != NULL; tmp = tmp->next){
+        fprintf(f, "( %s, %4d, %4d )\n",
+                mntbl[tmp->f].sym, tmp->l, tmp->a);
+    }
 }
 
-int makelabel(){
-  static int x = 0;
+int makelabel() {
+    static int x = 0;
 
-  x++;
-  return x;
+    x++;
+    return x;
 }
+
+// Cassin {{{
+void debug_print_code_h(cptr* c) {
+    code* tmp;
+
+    for(tmp=c->h; tmp != NULL; tmp = tmp->next){
+        printf("( %s, %4d, %4d )\n",
+                mntbl[tmp->f].sym, tmp->l, tmp->a);
+    }
+}
+void debug_print_code_t(cptr* c) {
+    code* tmp;
+
+    for(tmp=c->t; tmp != NULL; tmp = tmp->next){
+        printf("( %s, %4d, %4d )\n",
+                mntbl[tmp->f].sym, tmp->l, tmp->a);
+    }
+}
+// }}}
